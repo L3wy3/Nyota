dd86c48a83caf83a5899ba8d06bc3a980afe9e6c
Laurence Towning <lstowning@outlook.com>
2024-07-06 12:55:04 +0100

    Design tweraks

--- pages/woocommerce.vue
+++ pages/woocommerce.vue
@@ -1,76 +1,62 @@
 <script setup>
-// const url = "http://80.229.31.61/wordpress/wp-json/wc/v3/products"
-
 const config = useRuntimeConfig();
-// const url = "https://nyota-woo.co.uk/wordpress/wp-json/"
-// const oauth = OAuth({
-//       consumer: {
-//           key: '${config.wooKey}',
-//           secret: '${config.wooVal}'
-//       },
-//       signature_method: 'HMAC-SHA1'
-//   });
-
 const url5 = 'https://nyota-woo.co.uk/wordpress/wp-json/wc/v2/products?consumer_key=ck_3d18a89e470755860a070ddf2713861fe546a984&consumer_secret=cs_0d8c0ed68a74756f663971686d3fd4c0f3e3deef';
 const {data, error, pending} = await useFetch(url5, {
         method: 'GET',
-        // headers: {
-        //     "Authorization": "Basic" "Y2tfM2QxOGE4OWU0NzA3NTU4NjBhMDcwZGRmMjcxMzg2MWZlNTQ2YTk4NDpjc18wZDhjMGVkNjhhNzQ3NTZmNjYzOTcxNjg2ZDNmZDRjMGYzZTNkZWVmIg=="
-        // }
-        // credentials: 'include',
           auth: {
           username: 'ck_3d18a89e470755860a070ddf2713861fe546a984',
           password: 'cs_0d8c0ed68a74756f663971686d3fd4c0f3e3deef'
         }
       });
 
-//   const url5 = 'https://nyota-woo.co.uk/wordpress/wp-json/wc/v3/products';
-// const {data, error, pending} = await useFetch(url5, {
-//         method: 'GET',
-//         consumer: {
-//           public: ((config.wooKey)),
-//           secret: ((config.wooVal))
-//         },
-//   signature_method: 'HMAC-SHA1'
-//         // headers: {
-//         //     "Authorization": 
-//         //        oauth_consumer_key=((config.wooKey)),
-//         //        oauth_token=((config.wooVal)),
-//         //        oauth_signature_method="HMAC-SHA1"
-//         // }
-//       });
+function tagScrape(text) {
+      let regex = /(<([^>]+)>)/ig;
+      return text.replace(regex, "");
+}
 
-// const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
-// // import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api"; // Supports ESM
-// const url5 = 'https://nyota-woo.co.uk/wordpress/wp-json/wc/v3/products';
-// const {data} = await useFetch(url5, {
-// method: 'GET',
-// headers: {
-//     "Authorization": 'Bearer ' + ${config.wooKey}:${config.wooVal}
-//   }
-//   }
-// );
-// const WooCommerce = new WooCommerceRestApi({
-//   url: 'http://80.229.31.61/wordpress/wp-json/wc/v3/products', // Your store URL
-//   consumerKey: 'ck_3d18a89e470755860a070ddf2713861fe546a984', // Your consumer key
-//   consumerSecret: 'cs_0d8c0ed68a74756f663971686d3fd4c0f3e3deef', // Your consumer secret
-//   version: 'wc/v3' // WooCommerce WP REST API version
-// });
 
-// const { data, pending, error, refresh} = await useFetch(url)
-// http://80.229.31.61/wordpress/wp-json/wp/v2/pages
 </script>
-
+<style>
+#products-container {
+  max-width: 1600px;
+  padding: 20px;
+}
+.product-container {
+  width: 100%;
+  max-width: 300px;
+  position: relative;
+  border: 5px solid rgb(0, 110, 255);
+  border-radius: 5px;
+  margin: 5px;
+  padding: 20px
+}
+.product-images {
+  background-size: cover;
+  width: 100%;
+  height: 150px;
+}
+.sale-price {
+  background-color: red;
+  position: absolute;
+  top: -15px;
+  right: -15px;
+  height: 30px;
+  widows: 30px;
+}
+</style>
 <template>
-    <div>
+    <div id="products-container">
       <h1 class="bg-red-500">E-commerce data</h1>
       <div style="    display: flex;
     flex-direction: row;
     flex-wrap: wrap;
     justify-content: center;">
-        <div v-for="moo in data" style="background-color: red; margin: 10px; width: 200px">
-          <p>wuh</p>
-          <img :src=moo.images[0].src  style="width: 150px;"/>
+        <div class="product-container" v-for="moo in data">
+          <div class="product-images" :style="{'background-image': `url(${moo.images[0].src})`, 'width' : '150px', 'height' : '100px'}"></div>
+          <p>{{moo.name}}</p>
+          <p>{{ tagScrape(moo.description) }}</p>
+          <p>{{ moo.regular_price }}</p>
+          <p v-if="moo.sale_price" class="sale-price">{{ moo.sale_price }}</p>
         </div>
       </div>
       <!-- {{  data }} -->
