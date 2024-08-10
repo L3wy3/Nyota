449a94f8492b61e07f32b9e57e88d4f744cb7336
Laurence Towning <lstowning@outlook.com>
2024-08-11 00:19:27 +0100

    Adding hexagons

--- pages/index.vue
+++ pages/index.vue
@@ -15,24 +15,18 @@
         <img class="parallax_0" src="https://www.firewatchgame.com/images/parallax/parallax8.png" alt="">
     </section>
     <section class="content">
-        <h1>Title goes here</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, tenetur voluptatum temporibus nam voluptates, repudiandae soluta esse et quasi pariatur quia odit enim facilis perferendis neque maiores? In, officiis quam.
-        Odio nemo quidem suscipit dolor, expedita quos officiis consequuntur ducimus sed nobis, eveniet amet corporis aut at! Repellendus, illum laboriosam? Odio totam, illo at porro nesciunt provident vitae incidunt ex.
-        Harum dolor commodi repellendus odio atque cupiditate repudiandae, possimus sed nostrum cum perspiciatis iure vitae earum quisquam quaerat, amet unde voluptatum quia. Alias non, accusantium aliquam dignissimos iusto facilis autem.
-        Maiores fugiat nobis nemo tempora molestiae, eius fuga aut reprehenderit repudiandae facilis vel minus omnis consequuntur corporis deleniti iure impedit qui laborum dolorum enim minima in mollitia nesciunt. Ab, nihil.
-        Porro tenetur praesentium autem itaque repellendus consequuntur consequatur odit quidem, repellat quia quasi dolorem libero eveniet nisi sapiente asperiores aspernatur, sunt esse non reprehenderit reiciendis in beatae sint. Corrupti, ipsa?
-        Autem alias officiis voluptatem. Ipsam quod quasi corrupti fuga voluptatem rem hic provident molestiae odio quam. Laboriosam necessitatibus repellendus fuga est voluptatem, tempora reiciendis ea eos! Tempore rerum molestiae in!
-        Voluptate minus doloribus quod dolores. Laudantium reprehenderit molestiae animi earum ipsam eos minima natus expedita corrupti temporibus? Beatae quaerat autem fugit repellendus. Animi, repellat. Nobis, dignissimos ea. Error, nostrum dolorem.
-        Voluptatibus, ut vitae! Natus accusantium temporibus unde quasi veritatis vitae perferendis ducimus, molestiae numquam? Fugiat consequatur, eius eveniet totam sapiente necessitatibus maiores aliquid illum vero tenetur odit quidem quis obcaecati?
-        Eaque ipsum magni ab? Possimus nobis odio officiis incidunt earum reiciendis cupiditate nesciunt. Laboriosam esse dolorem impedit? Unde inventore dolorem itaque consequuntur esse dolores. Quia doloremque asperiores odit necessitatibus tenetur!
-        Expedita, maiores. Ab reprehenderit consectetur dolor incidunt quisquam placeat, debitis voluptatem dolore velit nobis numquam eius at nemo magnam cum aspernatur et accusantium aliquid repudiandae expedita officiis labore. Adipisci, tempora.
-        Recusandae saepe eum beatae. Libero recusandae vitae, nemo asperiores cum dolore optio illo omnis modi repellendus reiciendis quam itaque facilis. Tempora iure consectetur praesentium eos dicta quos, sed laboriosam. Quos!
-        Magni repellendus officiis perspiciatis est? Corrupti officiis perspiciatis velit a doloribus sint possimus, voluptate aliquid veniam perferendis sed quae eos, voluptatum dolorem harum neque autem maiores veritatis voluptatem. Sapiente, numquam?
-        Illum quos soluta eligendi reprehenderit a dicta autem neque magni error totam quisquam incidunt repellat eum quam dolores nihil animi similique, velit facilis, quae nostrum necessitatibus blanditiis aspernatur. Pariatur, ipsum?
-        Similique eaque animi reiciendis inventore fugiat veritatis, neque ab impedit libero sapiente quam sed cum at ratione aspernatur tempora ipsam quasi, quidem ipsa molestias dolorem voluptate. Quibusdam quia dignissimos ea!
-        Facilis, doloribus perspiciatis voluptas magni tempora tempore quod aperiam, quisquam sapiente atque eaque voluptatem dolorum culpa distinctio. Voluptatem sapiente atque, inventore est perferendis quia voluptates accusantium quos dicta in cupiditate?
-        Beatae architecto minus deserunt vel quisquam tempore perferendis corrupti nostrum error necessitatibus, rem quae recusandae numquam consequatur deleniti soluta laudantium animi fuga nesciunt? Earum similique dolorum delectus, doloremque natus reiciendis!
-        Sed in, quod, error tempora id neque minima illum nulla minus suscipit esse dolore quibusdam recusandae eius eaque ab possimus, debitis corrupti distinctio laudantium enim? A voluptatibus sapiente eos fugiat.
-        </p></section>
+        <h2>Previous projects</h2>
+        <p>I've been developing websites for almost a decade at this point, here are just some examples</p><br>
+        <article class="gallery">
+            <img src="https://assets.codepen.io/1506195/unsplash-music-0.avif" alt="guitar player at concert" />
+            <img src="https://assets.codepen.io/1506195/unsplash-music-1.avif" alt="duo singing" />
+            <img src="https://assets.codepen.io/1506195/unsplash-music-2.avif" alt="crowd cheering" />
+            <img src="https://assets.codepen.io/1506195/unsplash-music-3.avif" alt="singer performing" />
+            <img src="https://assets.codepen.io/1506195/unsplash-music-4.avif" alt="singer fistbumping crowd" />
+            <img src="https://assets.codepen.io/1506195/unsplash-music-5.avif" alt="man with a guitar singing" />
+            <img src="https://assets.codepen.io/1506195/unsplash-music-6.avif" alt="crowd looking at a lighted stage" />
+            <img src="https://assets.codepen.io/1506195/unsplash-music-7.avif" alt="woman singing on stage" />
+        </article>    
+    </section>
     </div>
 </template>
