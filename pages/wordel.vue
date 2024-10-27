620c095c0f684170feeaf591785fb71f32d633e0
Laurence Towning <lstowning@outlook.com>
2024-10-27 16:02:45 +0000

    wordle mostly working

--- pages/wordel.vue
+++ pages/wordel.vue
@@ -1,70 +1,98 @@
-<script setup lang="ts">
-import words from '../assets/fives.json'
-var row = 0;
-var column = 0;
-
-function keyStroke(key) {
-    items[2] = key
-    column++;
-    console.log("Row: "+row+" Column: "+" = "+key);
-    console.log(items[0])
-}
-const items = ["","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","",""];
-</script>
 
 <template>
     <div class="container">
         <div id="grid" class="grid">
-            <div v-for="item in items">
-                {{ item }}
+            <div v-for="item in items" :class="`${item[1] === 'green' ? 'green' : item[1] === 'yellow' ? 'yellow' : ''}`" >
+                {{ item[0] }}
             </div>
         </div>
         <div class="keyboard">
-            <div class="key" @click="keyStroke('q')">Q</div>
-            <div class="key" @click="keyStroke('w')">W</div>
-            <div class="key" @click="keyStroke('e')">E</div>
-            <div class="key" @click="keyStroke('r')">R</div>
-            <div class="key" @click="keyStroke('t')">T</div>
-            <div class="key" @click="keyStroke('y')">Y</div>
-            <div class="key" @click="keyStroke('u')">U</div>
-            <div class="key" @click="keyStroke('i')">I</div>
-            <div class="key" @click="keyStroke('o')">O</div>
-            <div class="key" @click="keyStroke('p')">P</div>
-            <div class="key" @click="keyStroke('a')">A</div>
-            <div class="key" @click="keyStroke('s')">S</div>
-            <div class="key" @click="keyStroke('d')">D</div>
-            <div class="key" @click="keyStroke('f')">F</div>
-            <div class="key" @click="keyStroke('g')">G</div>
-            <div class="key" @click="keyStroke('h')">H</div>
-            <div class="key" @click="keyStroke('j')">J</div>
-            <div class="key" @click="keyStroke('k')">K</div>
-            <div class="key" @click="keyStroke('l')">L</div>
-            <div class="key" @click="keyStroke('z')">Z</div>
-            <div class="key" @click="keyStroke('x')">X</div>
-            <div class="key" @click="keyStroke('c')">C</div>
-            <div class="key" @click="keyStroke('v')">V</div>
-            <div class="key" @click="keyStroke('b')">B</div>
-            <div class="key" @click="keyStroke('n')">N</div>
-            <div class="key" @click="keyStroke('m')">M</div>
+            <v-btn icon v-for="letter in alphabet" class="key" @click="setNewData(letter)">{{letter}}</v-btn>
         </div>
         <div>
-            <div @click="items[0] = 'f'">Enter</div>
-            <div>Back</div>
+            <button @click="EnterRow">Enter</button>
+            <button @click="DeleteChar" class="px-4 py-2 bg-pink-500 rounded uppercase font-bold hover:bg-pink-600 duration-300">Back</button>
         </div>
     </div>
+    <div>
+ </div>
     <client-only>
-    <p>{{ words[Math.floor(Math.random() * words.length)] }}</p>
+        <p>{{ word }}</p>
     </client-only>
-</template>
 
+</template>
+<script setup lang="ts">
+import type { ConstNode } from 'three/examples/jsm/nodes/Nodes.js';
+import words from '../assets/fives.json'
+const word = words[Math.floor(Math.random() * words.length)]
+var completedRow =0
+var winCondition = 0
+const EnterRow = () => {
+    if (count % 5 === 0 && count > completedRow*5) {
+        for (var i = 0; i < 5; i++) {
+            var letter = items.value[i+(completedRow*5)][0]
+            if(letter == word[i]) {
+                winCondition++
+                items.value[i+(completedRow*5)][1] = "green"
+            }
+        }
+        completedRow++
+        if(winCondition === 5) {
+            console.log("Congrats you won with "+(10-completedRow)+" lives remaining")
+        }
+    }
+    console.log(word)
+    return items
+}
+const setNewData = (letter: string) => {
+    if (count-(completedRow*5) < 5) {
+        console.log(letter);
+        // Set(items, 0, letter)
+        items.value[count][0] = letter
+        count++
+        return items
+    }
+    // make your changes here...
+  }
+var count = 0;
+var items = ref([["",0],["",0],["",0],["",0],["",0],["",0],["",0],["",0],["",0],["",0],["",0],["",0],["",0],["",0],["",0],["",0],["",0],["",0],["",0],["",0],["",0],["",0],["",0],["",0],["",0],["",0],["",0],["",0],["",0],["",0],["",0],["",0],["",0],["",0],["",0]]);
+const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","m","o","p","q","r","s","t","u","v","w","x","y","z"];
+const DeleteChar = () => {
+    if (count > completedRow*5) {
+        count--;
+        items.value[count][0] = "";
+        return items
+    }
+}
+
+import { useMagicKeys, whenever } from '@vueuse/core'
+const { shift, v, u, e, s, v_u_e, u_s_e, current } = useMagicKeys()
+const keys = computed(() => Array.from(current))
+whenever(keys, () => {
+  console.log(keys)
+})
+</script>
 <style>
 body {
 
 }
+
+.container {
+    margin-top: 200px;
+}
+div.yellow {
+    background-color: yellow;
+}
+div.green {
+    background-color: green;
+}
     .grid div {
         border: 1px solid #000;
         height: 50px;
-        width: 40px;
+        width: 50px;
+        font-size: 30px;
+        text-align: center;
+        text-transform: capitalize;
     }
     .grid {
         display: inline-grid;
